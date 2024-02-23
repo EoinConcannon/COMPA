21b2f368674601e27f57d4a00372579a9701c53b
EoinConcannon <G00406729@atu.ie>
2024-02-23 16:07:54 +0000

    added a welcome message, userInput is converted to lowercase

--- main/main.py
+++ main/main.py
@@ -1,10 +1,14 @@
 import numpy
 
-userInput = ""
-
-print("type \"exit\" to stop the program")
+print("COMPA:What is your name?")
+userInput = input("You:")
+print("COMPA:Hello " + userInput + ". I am COMPA, the comparison chatbot.")
+print("COMPA:I am in a very early stage of development right now.")
+print("COMPA:In the future, you will need to input two technologies for me to compare")
+print("COMPA:type \"exit\" to stop the program\n")
 
 while(userInput != "exit"):
     userInput = input("You:")
-    print("COMPA: " + userInput)
+    userInput = userInput.lower()#converts to lowercase
+    print("COMPA:" + userInput)
 
