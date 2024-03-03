c365565dca48fe698e291cade263348df38731c9
EoinConcannon <G00406729@atu.ie>
2024-03-03 13:49:00 +0000

    user input is now split up and read through a loop

--- main/main.py
+++ main/main.py
@@ -8,14 +8,17 @@ compareList = json.loads(compare)
 #print(compareList['response'])
 
 def processInput(userInput):
-    x = difflib.get_close_matches(userInput, compareList['compareReq'], 4, 0.6)
-    y = ''.join(x)
-    if(y == ""):#if user input doesn't match word in json "compareReq"
-        y = "what"
-    else:
-        y = random.choice(compareList['response'])#random choices give back random response from "response" in json
-    return y
+    for currentWord in userInput:
+        word = difflib.get_close_matches(currentWord, compareList['compareReq'], 4, 0.6)
+        ifWord = ''.join(word)
+        if(ifWord == ""):#if user input doesn't match word in json "compareReq"
+            continue
+        else:
+            ifWord = random.choice(compareList['response'])#random choices give back random response from "response" in json
+            return ifWord#have this go down into another function?
+    return "I don't understand"
 
+#welcome message
 print("COMPA:What is your name?")
 userInput = input("You:")
 print("COMPA:Hello " + userInput + ". I am COMPA, the comparison chatbot.")
@@ -26,5 +29,6 @@ print("COMPA:type \"exit\" to stop the program\n")
 while(userInput != "exit"):
     userInput = input("You:")
     userInput = userInput.lower()#converts to lowercase
+    userInput = userInput.split()
     botResponse = processInput(userInput)
     print("COMPA:" + botResponse)
