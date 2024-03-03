55b5d56f142bb21f670220c26c0677ffb0f5bca5
EoinConcannon <G00406729@atu.ie>
2024-03-03 15:20:45 +0000

    created new dictionary for holding tech, new function that reads this dictionary, changed var names

--- main/main.py
+++ main/main.py
@@ -3,19 +3,36 @@ import difflib
 import random
 import json
 
-compare = '{"compareReq": ["compare", "difference", "different", "differentiate", "contrast", "between"], "response": ["res1", "res2", "res3"]}'
-compareList = json.loads(compare)
-#print(compareList['response'])
+compareCMD = '{"compareCMD": ["compare", "difference", "different", "differentiate", "contrast", "between"]}'
+compareList = json.loads(compareCMD)
+tech = '{"techDatabase": ["tech1", "tech2", "tech3", "tech4"], "response": ["res1", "res2", "res3"]}'
+#create a better structure for tech JSON e.g. (Tech1: stat1 = 3525, stat2 = 6426) should be easy to work with/display
+techList = json.loads(tech)
 
 def processInput(userInput):
     for currentWord in userInput:
-        word = difflib.get_close_matches(currentWord, compareList['compareReq'], 4, 0.6)
+        word = difflib.get_close_matches(currentWord, compareList['compareCMD'], 4, 0.6)
         ifWord = ''.join(word)
-        if(ifWord == ""):#if user input doesn't match word in json "compareReq"
+        #print(currentWord)#DEBUG
+        if(ifWord == ""):#if user input doesn't match word in json "compareCMD"
             continue
         else:
-            ifWord = random.choice(compareList['response'])#random choices give back random response from "response" in json
-            return ifWord#have this go down into another function?
+            #stops reading list here
+            #moves into next function
+            ifWord = compareTech(userInput)
+            return ifWord
+    return "I don't understand"
+
+def compareTech(userInput):
+    for currentWord in userInput:
+        word = difflib.get_close_matches(currentWord, techList['techDatabase'], 4, 0.6)
+        ifWord = ''.join(word)
+        #print(currentWord)#DEBUG
+        if(ifWord == ""):#if user input doesn't match word in json "techDatabase"
+            continue
+        else:
+            ifWord = random.choice(techList['response'])#random choices give back random response from "response" in json
+            return ifWord
     return "I don't understand"
 
 #welcome message
