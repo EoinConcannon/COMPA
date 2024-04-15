77072586538e704b6d77457cd00eae2e9b8d39fd
EoinConcannon <G00406729@atu.ie>
2024-04-15 18:02:42 +0100

    fixed loop iteration, userInput and json key value can now be compared

--- main/main.py
+++ main/main.py
@@ -6,16 +6,13 @@ import json
 compareCmds = open("compareCmds.json")
 techDatabase = open("techDatabase.json")
 
-#create a better structure for tech JSON e.g. (Tech1: stat1 = 3525, stat2 = 6426) should be easy to work with/display
-compareList = json.load(compareCmds)
-techList = json.load(techDatabase)
+compareDict = json.load(compareCmds)
+techDict = json.load(techDatabase)
 
-def processInput(
-        userInput):
+def processInput(userInput):
     for currentWord in userInput:#loops through the user's string input
-        word = difflib.get_close_matches(currentWord, compareList['compareCMD'], 4, 0.6)#checks if the current word some what matchs a word in the json
+        word = difflib.get_close_matches(currentWord, compareDict['compareCMD'], 4, 0.6)#checks if the current word some what matchs a word in the json
         ifWord = ''.join(word)
-        #print(currentWord)#DEBUG
         if (ifWord == ""):#if user input doesn't match word in json "compareCMD"
             continue
         else:
@@ -25,35 +22,35 @@ def processInput(
             return ifWord
     return "I don't understand"
 
-def compareTech(
-        userInput):
+def compareTech(userInput):
     #checkers to make sure tech user has input is valid and matches the json
     twoCheck = 0
     tech1 = ""
     tech2 = ""
-    ifWord = ""
 
     for currentWord in userInput:
-        for key, value in techDatabase.item():#??????????????????
-            #if key == "name":
-            word = difflib.get_close_matches(currentWord, techList[i]['name'], 4, 0.6)
-            print(word)
-            ifWord = ''.join(word)
+        for techItem in techDict["tech"]:
+            for key, value in techItem.items():
+                ifWord = ""
+                if (key != "name"):
+                    continue
+
+                if (currentWord == value):#if user's input matches the key's name value
+                    ifWord = value
+                    
+                if (ifWord == ""):#if user input doesn't match word in json "techDatabase"
+                    continue
+                else:
+                    #ifWord = random.choice()#random choices give back random response from "response" in json
+                    #adds a json value to the variable, this is so it can be checked later
+                    #create a better system for this later
+                    if (twoCheck == 0):#first technology matches
+                        tech1 = ifWord
+                        twoCheck += 1
+                    elif (twoCheck == 1):#second technology matches
+                        tech2 = ifWord
+                        twoCheck += 1
         
-        #print(currentWord)#DEBUG
-        if (ifWord == ""):#if user input doesn't match word in json "techDatabase"
-            continue
-        else:
-            ifWord = random.choice(techList[1]['name'])#random choices give back random response from "response" in json
-            #adds a json value to the variable, this is so it can be checked later
-            #create a better system for this later
-            if (twoCheck == 0):#first technology matches
-                tech1 = ifWord
-                twoCheck += 1
-            elif (twoCheck == 1):#second technology matches
-                tech2 = ifWord
-                twoCheck += 1
-    
     if (twoCheck == 2):
         #list differences between specified tech
         print(tech1 + " " + tech2)
@@ -66,11 +63,11 @@ def compareTech(
         if (userInput == "-1"):#user doesn't give the name (won't be added to database)
             return "exiting"
         else:
-            techList.append(userInput)#name of tech added to database
+            #techList.append(userInput)#name of tech added to database
             #print(techList)
             print("Can you give me some additional information about this technology?")
             userInput = input("You:")
-            techList["response"] = userInput#description of tech added to database (this will be improved upon after changing how the json is structured)
+            #techList["response"] = userInput#description of tech added to database (this will be improved upon after changing how the json is structured)
             return "Thank you for telling me about this new tech"
     else:
         #user adds new tech to database
@@ -94,6 +91,5 @@ while(userInput != "-1"):
     if (userInput != "-1"):
         userInput = userInput.lower()#converts to lowercase
         userInput = userInput.split()#user's input converted to a list
-        print(techList) #unlabelled json python / https://realpython.com/python-dicts/
         botResponse = processInput(userInput)
         print("COMPA:" + botResponse)
